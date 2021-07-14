1681cd7bda0247c18fd9d27ac534621879fdafaa
wujiajin1 <86449962+wujiajin1@users.noreply.github.com>
2021-07-14 16:11:15 +0800

    hahahahahah

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"net/http"
 )
 
 // UserInfo 用户信息
 type UserInfo struct {
-	ID uint
 	Username string
 	Password string
 }
@@ -15,14 +15,39 @@ type UserInfo struct {
 
 func main() {
 	r:=gin.Default()
-	r.LoadHTMLFiles("./login.html")
+	r.LoadHTMLFiles("./login.html","./register.html")
 	r.GET("/login", func(c *gin.Context) {
 		c.HTML(200,"login.html",nil)
-		//username := c.Query("un")
-		//password := c.Query("pw")
-		//db:=connect()
-
 	})
-	//defer db.Close()
+	r.GET("/register", func(c *gin.Context) {
+		c.HTML(200,"register.html",nil)
+	})
+	r.POST("/register", func(c *gin.Context) {
+		username := c.PostForm("un")
+		password := c.PostForm("pw")
+		userinfo :=UserInfo{username,password}
+		db:=connect()
+		if db.Create(&userinfo).Error!=nil{
+			c.JSON(http.StatusBadRequest,gin.H{
+				"error":"user has been created",
+			})
+			return
+		}
+		c.JSON(http.StatusBadRequest,gin.H{
+			"status":"ok",
+		})
+		defer db.Close()
+	})
+	r.GET("/index", func(c *gin.Context) {
+	//	username := c.PostForm("un")
+	//	password := c.PostForm("pw")
+	//	userinfo :=UserInfo{username,password}
+	//var uu UserInfo
+	//	db:=connect()
+	//	if db.Where("username=?", username).Find(&userinfo)!=nil{
+	//	fmt.Println(db.Where(&UserInfo{Username: "p93002612", Password: "WJJ99zyh"}).Find(&uu).Error)
+	//	}
+	//	defer db.Close()
+	})
 	r.Run(":8080")
 }
