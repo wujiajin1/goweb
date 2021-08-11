30d9707b11ff148f2a35c900c2f9d7c4fab7a985
wujiajin <83448614@qq.com>
2021-08-11 17:55:34 +0800

    update2

--- main.go
+++ main.go
@@ -5,72 +5,114 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"strconv"
 )
 
-
-
-
 func main() {
-	r:=gin.Default()
+	db:= connect()
+	for i := 60; i <90 ; i++ {
+		b:=BlogsInfo{
+			Title: string(i)+string(i),
+			Text:  string(i)+string(i)+string(i)+string(i)+string(i)+string(i)+string(i)+string(i)+string(i)+string(i)+string(i)+string(i),
+			PS:    string(i)+string(i)+string(i)+string(i)+string(i),
+		}
+		db.Create(&b)
+	}
+	var username, password string
+	r := gin.Default()
 	r.Static("/statics", "./statics")
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/login", func(c *gin.Context) {
-		c.HTML(200,"login.html",gin.H{
-			"code":0,
+		c.HTML(200, "login.html", gin.H{
+			"code": 0,
 		})
 	})
-	r.POST("/login", func(c *gin.Context) {
-		db:=connect()
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-		var uu = UserInfo{}
-		result:=db.Find(&uu, "username=? AND password=?",username,password)
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			c.HTML(200, "login.html", gin.H{
-				"code":1,
-			})
-		}else{
-			c.HTML(200,"main.html", gin.H{
-				"code":2,
+	r.GET("/main", func(c *gin.Context) {
+		if username != "" {
+			c.HTML(200, "main.html", gin.H{
+				"code": 1,
 				"usr":username,
+			})
+		} else {
+			c.HTML(200, "main.html", gin.H{
+				"code": 2,
+			})
+		}
+	})
+	r.POST("/main", func(c *gin.Context) {
+		db := connect()
+		username = c.PostForm("username")
+		password = c.PostForm("password")
+		var uu = UserInfo{}
+		result := db.Find(&uu, "username=? AND password=?", username, password)
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			username,password="",""
+			c.HTML(200, "login.html", gin.H{
+				"code": 1,
+			})
+		} else {
+			c.HTML(200, "main.html", gin.H{
+				"code": 2,
+				"usr":  username,
 			})
 		}
 	})
 	r.GET("/register", func(c *gin.Context) {
-		c.HTML(200,"register.html",gin.H{
-			"code":0,
+		c.HTML(200, "register.html", gin.H{
+			"code": 0,
 		})
 	})
 	r.POST("/register", func(c *gin.Context) {
-		username := c.PostForm("username")
-		password := c.PostForm("password")
+		rigiName := c.PostForm("username")
+		rigiPassword := c.PostForm("password")
 		var uu = UserInfo{}
-		db:=connect()
-		result:=db.Find(&uu, "username=?",username)
+		db := connect()
+		result := db.Find(&uu, "username=?", username)
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			userinfo :=UserInfo{username,password}
+			userinfo := UserInfo{rigiName, rigiPassword}
 			db.Create(&userinfo)
-			c.HTML(200,"register.html",gin.H{
-				"code":2,
+			c.HTML(200, "register.html", gin.H{
+				"code": 2,
 			})
-		}else{
-			c.HTML(200,"register.html",gin.H{
-				"code":1,
+		} else {
+			c.HTML(200, "register.html", gin.H{
+				"code": 1,
 			})
 		}
 		defer db.Close()
 	})
 	r.GET("/write", func(c *gin.Context) {
-		c.HTML(200,"writeblog.html",nil)
+		if username != "" {
+			c.HTML(200, "writeblog.html", gin.H{
+				"code": 1,
+				"usr":username,
+			})
+		} else {
+			c.HTML(200, "writeblog.html", gin.H{
+				"code": 2,
+			})
+		}
 	})
-	r.POST("write", func(c *gin.Context) {
-		title  := c.PostForm("title")
+	r.POST("/write", func(c *gin.Context) {
+		title := c.PostForm("title")
 		text := c.PostForm("text")
 		ps := c.PostForm("ps")
-		db:=connect()
-		bloginfo:=BlogsInfo{title,text,ps}
+		db := connect()
+		bloginfo := BlogsInfo{title, text, ps}
 		db.Create(&bloginfo)
-		c.HTML(200,"main.html",nil)
+		c.HTML(200, "main.html", gin.H{
+			"code": 0,
+			"usr":  username,
+		})
+	})
+	r.GET("/blogs", func(c *gin.Context) {
+		p, _ := strconv.Atoi(c.Query("page"))
+		I,BI:=pageDivision(p)
+		c.HTML(200,"blogs.html",gin.H{
+			"usr":username,
+			"data":BI,
+			"page":I,
+		})
 	})
 	r.Run(":8080")
 }
---
+++ pageDivision.go
@@ -0,0 +1,51 @@
+package main
+
+func pageDivision(p int)(I1 []int, B1 []BlogsInfo){
+	db := connect()
+	var B []BlogsInfo
+	var b BlogsInfo
+	var i int
+	var I []int
+	result := db.Find(&b)
+	q := int(result.RowsAffected)
+	result = db.Limit(10).Offset((p - 1) * 10).Find(&b)
+	rows, _ := result.Rows()
+	if p*10-q > 0 {
+		for j := 0; j < 10-(p*10-q); j++ {
+			rows.Next()
+			db.ScanRows(rows, &b)
+			B = append(B, b)
+		}
+	} else {
+		for j := 0; j < 10; j++ {
+			rows.Next()
+			db.ScanRows(rows, &b)
+			B = append(B, b)
+		}
+	}
+	if q > 60 {
+		if p <= 3 {
+			p = 4
+		}
+		if p > q/10-3 && q%10==0{
+			p = q/10 - 3
+		}
+		if p > q/10-3 && q%10!=0{
+			p = q/10 - 2
+		}
+		for i = p - 3; i < p+4; i++ {
+			I = append(I, i)
+		}
+	} else {
+		if q%10 == 0 {
+			for i = 1; i <= q/10; i++ {
+				I = append(I, i)
+			}
+		} else {
+			for i = 1; i <= q/10+1; i++ {
+				I = append(I, i)
+			}
+		}
+	}
+	return I, B
+}
